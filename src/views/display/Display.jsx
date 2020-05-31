d7422ff34de3fd5c3fa88d4baa2625c4a18f1084
edwinmoradian90 <edwin.moradian90@gmail.com>
2020-05-30 23:18:25 -0700

    added basic structure

---
+++ src/views/display/Display.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Header from '../../components/header/Header'
+
+function Display(props) {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Display
